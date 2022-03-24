46bfd05fc3c08057d6b01cc26c118781f9ff2c30
sinan14 <sinuzar5@gmail.com>
2022-03-24 14:24:37 +0530

    final commit

---
+++ Y/src/app/list/list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { GameService } from '../game.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
+})
+export class ListComponent implements OnInit {
+  constructor(public _service: GameService) {}
+  noOFGames: number = 0;
+  currentPage: number = 1;
+  lastPage: number = 1;
+  total: number = 1;
+  ngOnInit(): void {
+    this.noOFGames = this._service.games.length;
+    this.total = Math.ceil(this.noOFGames / 10);
+    this.lastPage = this.total;
+    console.log(this.total, this.noOFGames);
+  }
+
+  public onPageChange(page) {
+    this.currentPage = page;
+    console.log(page);
+    const limit = 10;
+    const skip = (page - 1) * limit;
+    this._service.games1 = this._service.games(skip, skip + 10);
+  }
+}
